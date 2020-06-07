b7cde4f3c0eeb3c1c5f5d78be0313878b2271539
JuanebsJ8 <66316667+JuanebsJ8@users.noreply.github.com>
2020-06-07 13:44:11 -0500

    Update and rename Estadisticas.py to Distribuciones.py

--- Distribuciones.py
+++ Distribuciones.py
@@ -3,9 +3,12 @@ PROVENGAN DE LO INGRESADO POR EL USUARIO Y NO LAS DE PRUEBA QUE DEJE ESCRITAS
 ##Primeras 14 Lineas Estetica de Los Modulos##
 # importando modulos necesarios
 
+import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np 
-from scipy import stats 
+from scipy import stats
+import pandas as pd
+from scipy.optimize import fsolve
+from math import e
 import seaborn as sns 
 
 np.random.seed(2016) # replicar random
@@ -19,21 +22,9 @@ sns.set_context(rc={"figure.figsize": (8, 4)})
 ###Desde aqui las variables son en referencia a como se valla a desarrollar el proyecto y por eso las deje entre ##
 
 
-##Distribucion Normal
-# Graficando histograma
-mu, sigma = 0, 0.2 # media y desvio estandar
-datos = np.random.normal(mu, sigma, 1000) #creando muestra de datos
-# histograma de distribución normal.
-cuenta, cajas, ignorar = plt.hist(datos, 20)
-plt.ylabel('frequencia')
-plt.xlabel('valores')
-plt.title('Histograma')
-plt.show()
-
-
 ##Distribucion Binomial e Historiagrama
 # Graficando Binomial
-N, p = 30, 0.4 # parametros de forma 
+N, p = float(input('Ingrese N')),float(input('ingrese P')) # parametros de forma 
 binomial = stats.binom(N, p) # Distribución
 x = np.arange(binomial.ppf(0.01),
               binomial.ppf(0.99))
@@ -55,7 +46,7 @@ plt.show()
 
 ##Distribucion geometrica
 # Graficando Geométrica
-p =  0.3 # parametro de forma 
+p =  float(input('Ingrese la Probabilidad')) # parametro de forma 
 geometrica = stats.geom(p) # Distribución
 x = np.arange(geometrica.ppf(0.01),
               geometrica.ppf(0.99))
@@ -76,7 +67,7 @@ plt.show()
 
 ##Distribucion Hipergeometrica
 # Graficando Hipergeométrica
-M, n, N = 30, 10, 12 # parametros de forma 
+M, n, N = float(input('Ingrese M')), float(input('Ingrese n')), float(input('Ingrese N')) # parametros de forma 
 hipergeometrica = stats.hypergeom(M, n, N) # Distribución
 x = np.arange(0, n+1)
 fmp = hipergeometrica.pmf(x) # Función de Masa de Probabilidad
@@ -97,7 +88,7 @@ plt.show()
 
 ##Distribucion de Bernoulli
 # Graficando Bernoulli
-p =  0.5 # parametro de forma 
+p =  float(input('Ingrese p')) # parametro de forma 
 bernoulli = stats.bernoulli(p)
 x = np.arange(-1, 3)
 fmp = bernoulli.pmf(x) # Función de Masa de Probabilidad
@@ -119,7 +110,7 @@ plt.show()
 
 ##Distribucion de Poisson
 # Graficando Poisson
-mu =  3.6 # parametro de forma 
+mu =  float(input('Ingrese Miu')) # parametro de forma 
 poisson = stats.poisson(mu) # Distribución
 x = np.arange(poisson.ppf(0.01),
               poisson.ppf(0.99))
@@ -137,24 +128,3 @@ plt.ylabel('frequencia')
 plt.xlabel('valores')
 plt.title('Histograma Poisson')
 plt.show()
-
-
-##De la Normal
-# Graficando Normal
-mu, sigma = 0, 0.2 # media y desvio estandar
-normal = stats.norm(mu, sigma)
-x = np.linspace(normal.ppf(0.01),
-                normal.ppf(0.99), 100)
-fp = normal.pdf(x) # Función de Probabilidad
-plt.plot(x, fp)
-plt.title('Distribución Normal')
-plt.ylabel('probabilidad')
-plt.xlabel('valores')
-plt.show()
-# histograma
-aleatorios = normal.rvs(1000) # genera aleatorios
-cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
-plt.ylabel('frequencia')
-plt.xlabel('valores')
-plt.title('Histograma Normal')
-plt.show(
