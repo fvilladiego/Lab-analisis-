57ce9a3627f0687f3fe74e28d80346191619a634
JuanebsJ8 <66316667+JuanebsJ8@users.noreply.github.com>
2020-06-07 02:08:51 -0500

    Add files via upload

---
+++ Estadisticas.py
@@ -0,0 +1,199 @@
+##Primeras 14 Lineas Estetica de Los Modulos##
+# importando modulos necesarios
+
+import matplotlib.pyplot as plt
+import numpy as np 
+from scipy import stats 
+import seaborn as sns 
+
+np.random.seed(2016) # replicar random
+
+# parametros esteticos de seaborn
+sns.set_palette("deep", desat=.6)
+sns.set_context(rc={"figure.figsize": (8, 4)})
+##Historiagramas##
+
+
+###Desde aqui las variables son en referencia a como se valla a desarrollar el proyecto y por eso las deje entre ##
+
+
+##Distribucion Normal
+# Graficando histograma
+mu, sigma = 0, 0.2 # media y desvio estandar
+datos = np.random.normal(mu, sigma, 1000) #creando muestra de datos
+# histograma de distribución normal.
+cuenta, cajas, ignorar = plt.hist(datos, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma')
+plt.show()
+
+
+##Distribucion Binomial e Historiagrama
+# Graficando Binomial
+N, p = 30, 0.4 # parametros de forma 
+binomial = stats.binom(N, p) # Distribución
+x = np.arange(binomial.ppf(0.01),
+              binomial.ppf(0.99))
+fmp = binomial.pmf(x) # Función de Masa de Probabilidad
+plt.plot(x, fmp, '--')
+plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+plt.title('Distribución Binomial')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = binomial.rvs(1000)  # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Binomial')
+plt.show()
+
+
+##Distribucion geometrica
+# Graficando Geométrica
+p =  0.3 # parametro de forma 
+geometrica = stats.geom(p) # Distribución
+x = np.arange(geometrica.ppf(0.01),
+              geometrica.ppf(0.99))
+fmp = geometrica.pmf(x) # Función de Masa de Probabilidad
+plt.plot(x, fmp, '--')
+plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+plt.title('Distribución Geométrica')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = geometrica.rvs(1000)  # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Geométrica')
+plt.show()
+
+##Distribucion Hipergeometrica
+# Graficando Hipergeométrica
+M, n, N = 30, 10, 12 # parametros de forma 
+hipergeometrica = stats.hypergeom(M, n, N) # Distribución
+x = np.arange(0, n+1)
+fmp = hipergeometrica.pmf(x) # Función de Masa de Probabilidad
+plt.plot(x, fmp, '--')
+plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+plt.title('Distribución Hipergeométrica')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = hipergeometrica.rvs(1000)  # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Hipergeométrica')
+plt.show()
+
+
+##Distribucion de Bernoulli
+# Graficando Bernoulli
+p =  0.5 # parametro de forma 
+bernoulli = stats.bernoulli(p)
+x = np.arange(-1, 3)
+fmp = bernoulli.pmf(x) # Función de Masa de Probabilidad
+fig, ax = plt.subplots()
+ax.plot(x, fmp, 'bo')
+ax.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+ax.set_yticks([0., 0.2, 0.4, 0.6])
+plt.title('Distribución Bernoulli')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = bernoulli.rvs(1000)  # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Bernoulli')
+plt.show()
+
+##Distribucion de Poisson
+# Graficando Poisson
+mu =  3.6 # parametro de forma 
+poisson = stats.poisson(mu) # Distribución
+x = np.arange(poisson.ppf(0.01),
+              poisson.ppf(0.99))
+fmp = poisson.pmf(x) # Función de Masa de Probabilidad
+plt.plot(x, fmp, '--')
+plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+plt.title('Distribución Poisson')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = poisson.rvs(1000)  # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Poisson')
+plt.show()
+
+
+##De la Normal
+# Graficando Normal
+mu, sigma = 0, 0.2 # media y desvio estandar
+normal = stats.norm(mu, sigma)
+x = np.linspace(normal.ppf(0.01),
+                normal.ppf(0.99), 100)
+fp = normal.pdf(x) # Función de Probabilidad
+plt.plot(x, fp)
+plt.title('Distribución Normal')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = normal.rvs(1000) # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Normal')
+plt.show(
+
+##Distribucion Continua
+# Graficando Uniforme
+uniforme = stats.uniform()
+x = np.linspace(uniforme.ppf(0.01),
+                uniforme.ppf(0.99), 100)
+fp = uniforme.pdf(x) # Función de Probabilidad
+fig, ax = plt.subplots()
+ax.plot(x, fp, '--')
+ax.vlines(x, 0, fp, colors='b', lw=5, alpha=0.5)
+ax.set_yticks([0., 0.2, 0.4, 0.6, 0.8, 1., 1.2])
+plt.title('Distribución Uniforme')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = uniforme.rvs(1000) # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Uniforme')
+plt.show()
+
+##Exponencial
+# Graficando Exponencial
+exponencial = stats.expon()
+x = np.linspace(exponencial.ppf(0.01),
+                exponencial.ppf(0.99), 100)
+fp = exponencial.pdf(x) # Función de Probabilidad
+plt.plot(x, fp)
+plt.title('Distribución Exponencial')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = exponencial.rvs(1000) # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Exponencial')
+plt.show()
