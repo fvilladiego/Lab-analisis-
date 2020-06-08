ed71bf267e7d125df07a93b9661eb3220faff89e
JuanebsJ8 <66316667+JuanebsJ8@users.noreply.github.com>
2020-06-08 13:10:14 -0500

    Update Distribuciones.py

--- Distribuciones.py
+++ Distribuciones.py
@@ -148,3 +148,24 @@ plt.ylabel('frequencia')
 plt.xlabel('valores')
 plt.title('Histograma Normal')
 plt.show()
+
+
+##Distribucion Exponencial
+# Graficando Exponencial
+a=float(input('Ingrese a '))
+exponencial = stats.expon(a)
+x = np.linspace(exponencial.ppf(0.01),
+                exponencial.ppf(0.99), 100)
+fp =exponencial.pdf(x) # Función de Probabilidad
+plt.plot(x, fp)
+plt.title('Distribución Exponencial')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = exponencial.rvs(1000) # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Exponencial')
+plt.show()
