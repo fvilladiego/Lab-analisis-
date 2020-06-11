9eb25c0d51a7481359f5f0df520013d8f2041b76
JuanebsJ8 <66316667+JuanebsJ8@users.noreply.github.com>
2020-06-11 07:25:26 -0500

    Update Distribuciones.py

--- Distribuciones.py
+++ Distribuciones.py
@@ -169,3 +169,26 @@ plt.ylabel('frequencia')
 plt.xlabel('valores')
 plt.title('Histograma Exponencial')
 plt.show()
+
+
+##Distribucion Uniforme
+# Graficando Uniforme
+uniforme = stats.uniform()
+x = np.linspace(uniforme.ppf(0.01),
+                uniforme.ppf(0.99), 100)
+fp = uniforme.pdf(x) # Función de Probabilidad
+fig, ax = plt.subplots()
+ax.plot(x, fp, '--')
+ax.vlines(x, 0, fp, colors='b', lw=5, alpha=0.5)
+ax.set_yticks([0., 0.2, 0.4, 0.6, 0.8, 1., 1.2])
+plt.title('Distribución Uniforme')
+plt.ylabel('probabilidad')
+plt.xlabel('valores')
+plt.show()
+# histograma
+aleatorios = uniforme.rvs(1000) # genera aleatorios
+cuenta, cajas, ignorar = plt.hist(aleatorios, 20)
+plt.ylabel('frequencia')
+plt.xlabel('valores')
+plt.title('Histograma Uniforme')
+plt.show()
